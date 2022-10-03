9af81b648f69ed8e46cc8e69663a5079f9e86639
Goodness Obi <tope@DESKTOP-NCM2GKD.localdomain>
2022-10-03 22:40:54 +0100

    data modelling and schema setup

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@chakra-ui/layout'
+import React from 'react';
 import Sidebar from './sidebar';
 
 const PlayerLayout = ({ children}) => {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import 'reset-css'
 import PlayerLayout from '../components/playerLayout'
